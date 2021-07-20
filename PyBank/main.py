171d17e0bfe7b9cba86f56a49b9d838d43e4be96
Daniella Mayoral <daniellamayoral@Daniellas-MacBook-Pro.local>
2021-07-19 21:01:48 -0500

    Creating First main file for PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,25 @@
+#Note most of code was provided by the UTSA instructor(Jeff Anderson) just modify to fit the assignment.
+
+#Impot os module and reading CSV modules
+import os
+import csv
+
+#locating the file path of reading the csv data
+PyBankdataFile = os.path.join('Resources', 'budget_data.csv')
+
+#Opens and reads csv file
+with open(PyBankdataFile, newline="") as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    
+    #checking if this prints the Header of the CSV file.
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+
+        print(row)
